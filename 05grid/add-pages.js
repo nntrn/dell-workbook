35e644fecf0a2255d60ffe8f95b668104b1b605a
annie tran <nntrn@me.com>
2018-11-12 13:44:03 -0600

    more uniform dummy text

--- 05grid/add-pages.js
+++ 05grid/add-pages.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
   for (i = 1; i < 11; i++) {
-    $( ".grid-"+i ).load( "https://nntrn.github.io/programming-notes/resources/html/flex-template.html");
-  }
-  });
+    $(".grid-" + i).load("https://nntrn.github.io/programming-notes/resources/html/flex-template.html");
+}});
