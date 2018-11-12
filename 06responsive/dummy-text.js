5b86820168a0b5083a97b850780fa8ead0e9e451
annie tran <nntrn@me.com>
2018-11-12 08:28:25 -0600

    responsive &#0033;

---
+++ 06responsive/dummy-text.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+  for (i = 1; i < 11; i++) {
+    $(".grid-" + i).prepend('<div class=container><div class=image><a class=focus href=#><img src=https://placeimg.com/640/480/any></a></div><div class=card><div class=section-title><h1>' + lorem.ipsum('w4') + '</h1></div><div class=caption>' + lorem.ipsum('p6') + '</div><div class=post-links><div class=likes><a href=#>&hearts;' + Math.floor(Math.random() * Math.floor(99)) +'</a></div><div class=read-more><a href=#>Read More</a></div></div>');
+  }
+
+  $('.caption').readmore({
+    moreLink: '<a href="#">Read more</a>',
+    collapsedHeight: 200,
+    afterToggle: function (trigger, element, expanded) {
+      if (!expanded) { // The "Close" link was clicked
+        $('html, body').animate({
+          scrollTop: element.offset().top
+        }, {
+          duration: 100
+        });
+      }
+    }
+  });
+
+
+
+
+});
